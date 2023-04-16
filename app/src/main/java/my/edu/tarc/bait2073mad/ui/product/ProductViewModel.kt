372ea4e58db6eb1ffd41c5b5f5fabe7f59b67e28
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2023-04-16 21:38:31 +0800

    Add Product and Search

---
+++ app/src/main/java/my/edu/tarc/bait2073mad/ui/product/ProductViewModel.kt
@@ -0,0 +1,7 @@
+package my.edu.tarc.bait2073mad.ui.product
+
+import androidx.lifecycle.ViewModel
+
+class ProductViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/my/edu/tarc/bait2073mad/ui/search/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package my.edu.tarc.bait2073mad.ui.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
