84ba97f93923be0a489b5fc039e4e1b510ae3896
Eng Lip <englip455@gmail.com>
2023-04-16 22:33:05 +0800

    cart

---
+++ app/src/main/java/my/edu/tarc/bait2073mad/ui/cart/item.java
@@ -0,0 +1,37 @@
+package my.edu.tarc.bait2073mad.ui.cart;
+
+public class item {
+    String productName;
+    String productPrice;
+    int productQuantity;
+
+    public item(String productName, String productPrice, int productQuantity) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productQuantity = productQuantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+}
