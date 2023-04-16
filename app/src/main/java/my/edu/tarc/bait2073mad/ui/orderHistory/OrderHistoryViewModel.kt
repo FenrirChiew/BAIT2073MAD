3836fe3c372f9c3fd1b3686e478492820e1de0c0
Eng Lip <englip455@gmail.com>
2023-04-16 23:11:24 +0800

    orderhistory

---
+++ app/src/main/java/my/edu/tarc/bait2073mad/ui/orderHistory/OrderHistoryViewModel.kt
@@ -0,0 +1,7 @@
+package my.edu.tarc.bait2073mad.ui.orderHistory
+
+import androidx.lifecycle.ViewModel
+
+class OrderHistoryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/my/edu/tarc/bait2073mad/ui/orderHistory/orderHistory.kt
@@ -0,0 +1,32 @@
+package my.edu.tarc.bait2073mad.ui.orderHistory
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import my.edu.tarc.bait2073mad.R
+
+class orderHistory : Fragment() {
+
+    companion object {
+        fun newInstance() = orderHistory()
+    }
+
+    private lateinit var viewModel: OrderHistoryViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_order_history, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OrderHistoryViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
