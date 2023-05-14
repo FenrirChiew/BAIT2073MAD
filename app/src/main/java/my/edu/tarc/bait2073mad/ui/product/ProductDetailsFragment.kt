8959603014f84e0cca6b3dd3bf17742c45cdff66
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2023-05-14 13:44:30 +0800

    Update Products

--- app/src/main/java/my/edu/tarc/bait2073mad/ui/product/ProductDetailsFragment.kt
+++ app/src/main/java/my/edu/tarc/bait2073mad/ui/product/ProductDetailsFragment.kt
@@ -1,5 +1,7 @@
 package my.edu.tarc.bait2073mad.ui.product
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,6 +18,8 @@ import my.edu.tarc.bait2073mad.R
 import my.edu.tarc.bait2073mad.databinding.FragmentProductDetailsBinding
 import my.edu.tarc.bait2073mad.ui.cart.CartViewModel
 import my.edu.tarc.bait2073mad.ui.home.HomeViewModel
+import java.io.File
+import java.io.FileNotFoundException
 
 class ProductDetailsFragment : Fragment(), MenuProvider {
 
@@ -44,18 +48,23 @@ class ProductDetailsFragment : Fragment(), MenuProvider {
         val productID = homeViewModel.productList.value!![homeViewModel.selectedIndex].productID
         val productIndex = productID.substring(productID.length - 4).toInt()
         val productSelected = homeViewModel.productList.value!![productIndex]
-
+        val productImage = readProductImage(productID.lowercase().plus(".jpg"))
+        if (productImage != null) {
+            _binding?.imageViewProductImage?.setImageBitmap(productImage)
+        } else {
+            _binding?.imageViewProductImage?.setImageResource(R.drawable.ic_product_black_24dp)
+        }
         _binding?.textViewProductName?.text = productSelected.productName
         _binding?.textViewProductPrice?.text = getString(R.string.currency_myr).plus(" ")
             .plus("%.2f".format(productSelected.productPrice))
         _binding?.textViewProductStatus?.text = productSelected.productStatus
         _binding?.textViewProductSeller?.text = productSelected.seller
+        _binding?.textViewProductDescription?.text = productSelected.productDescriptions
         if (productSelected.favorite) {
             _binding?.imageViewFavorite?.setImageResource(R.drawable.ic_favorite_red_24dp)
         } else {
             _binding?.imageViewFavorite?.setImageResource(R.drawable.ic_favorite_border_red_24dp)
         }
-        _binding?.textViewProductDescription?.text = productSelected.productDescriptions
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
@@ -67,5 +76,18 @@ class ProductDetailsFragment : Fragment(), MenuProvider {
             findNavController().navigateUp()
         }
         return true
+    }
+
+    private fun readProductImage(filename: String): Bitmap? {
+        val file = File(this.context?.filesDir, filename)
+
+        if (file.isFile) {
+            try {
+                return BitmapFactory.decodeFile(file.absolutePath)
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            }
+        }
+        return null
     }
 }
