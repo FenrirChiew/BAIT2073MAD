a3cfa61a6986192df05c96489dc936521f245e13
yeesoon12 <86587298+yeesoon12@users.noreply.github.com>
2023-05-14 13:17:22 +0800

    Account Update

--- app/src/main/java/my/edu/tarc/bait2073mad/ui/account/AccountFragment.kt
+++ app/src/main/java/my/edu/tarc/bait2073mad/ui/account/AccountFragment.kt
@@ -1,21 +1,26 @@
 package my.edu.tarc.bait2073mad.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 import my.edu.tarc.bait2073mad.databinding.FragmentAccountBinding
+import my.edu.tarc.bait2073mad.ui.Login
 
 class AccountFragment : Fragment() {
 
     private var _binding: FragmentAccountBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+    val db = FirebaseFirestore.getInstance()
+    private lateinit var docRef: DocumentReference
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,13 +31,54 @@ class AccountFragment : Fragment() {
             ViewModelProvider(this)[AccountViewModel::class.java]
 
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        auth = FirebaseAuth.getInstance()
+        val userID = auth.currentUser?.uid
+        val username: String
+        val userEmail = auth.currentUser?.email
+        docRef = db.collection("users").document(userID!!)
 
-        val textView: TextView = binding.textAccount
-        accountViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        docRef.get()
+            .addOnSuccessListener { doc ->
+                if (doc != null && doc.exists()) {
+                    profileData = doc.data
+                    username =
+                        profileData.get("username") as String
+                    binding.usernameTextView.text = username
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(requireContext(), "Data Read Failed", Toast.LENGTH_SHORT).show()
+            }
+
+
+        binding.buttonTestUpload.setOnClickListener {
+            profileData = hashMapOf(
+                "username" to username,
+                "email" to userEmail
+            )
+
+            docRef.set(profileData)
+                .addOnSuccessListener {
+                    Toast.makeText(requireContext(), "Update Successfully", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(requireContext(), "Update Failed", Toast.LENGTH_SHORT).show()
+                }
         }
-        return root
+
+        binding.buttonLogout.setOnClickListener {
+            auth.signOut()
+
+            val intent = Intent(requireContext(), Login::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+        accountViewModel.text.observe(viewLifecycleOwner) {
+
+        }
+        return binding.root
     }
 
     override fun onDestroyView() {
